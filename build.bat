2f57bca72b7976ae5e59e8d5ca85fe8b6760417c
Yao Min <ymin@ezesoft.com>
2017-07-11 18:33:52 -0400

    try

--- build.bat
+++ build.bat
@@ -1,5 +1,2 @@
 cd Build_Win32
 msbuild /t:Rebuild /p:Configuration=Release;Platform=Win32 /m cpp_redis.sln
-
-cd ..\Build_Win64
-msbuild /t:Rebuild /p:Configuration=Release;Platform=x64 /m cpp_redis.sln
