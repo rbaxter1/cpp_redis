3dc787d6054121958716ce8ce43925ed3311c9d1
Yao Min <ymin@ezesoft.com>
2017-07-11 18:18:38 -0400

    everything

--- build.bat
+++ build.bat
@@ -1,10 +1,5 @@
-mkdir Build_Win32
 cd Build_Win32
-cmake ../ -G "Visual Studio 12 2013"
 msbuild /t:Rebuild /p:Configuration=Release;Platform=Win32 /m cpp_redis.sln
 
-cd ..
-mkdir Build_x4
-cd Build_x64
-cmake ../ -G "Visual Studio 12 2013 Win64"  
+cd ..\Build_Win64
 msbuild /t:Rebuild /p:Configuration=Release;Platform=x64 /m cpp_redis.sln
